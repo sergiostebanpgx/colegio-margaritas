5a3f999b4a556fd09c9b3847d6f3058096f0852d
sergiostebanpgx <sergiosteban700@gmail.com>
2025-02-21 13:03:21 -0500

    Create component AnimationIcons

---
+++ app/components/AnimationIcons.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import Lottie from "lottie-react";
+
+interface AnimationIconsProps {
+  type:
+    | "mail"
+    | "register"
+    | "pdf"
+    | "call"
+    | "location"
+    | "blog"
+    | "search"
+    | "newspaper"; // Asegurar que `type` es uno de estos valores
+  size?: number; // Asegurar que `size` es un número
+}
+
+const AnimationIcons: React.FC<AnimationIconsProps> = ({
+  type,
+  size = 100,
+}) => {
+  const [animationData, setAnimationData] = useState<any>(null);
+
+  useEffect(() => {
+    let animationFile = "";
+    switch (type) {
+      case "mail":
+        animationFile = "/animations/mail.json";
+        break;
+      case "register":
+        animationFile = "/animations/Register.json";
+        break;
+      case "pdf":
+        animationFile = "/animations/pdf.json";
+        break;
+      case "call":
+        animationFile = "/animations/call.json";
+        break;
+      case "location":
+        animationFile = "/animations/location.json";
+        break;
+      case "blog":
+        animationFile = "/animations/blog.json";
+        break;
+      case "search":
+        animationFile = "/animations/search.json";
+        break;
+      case "newspaper":
+        animationFile = "/animations/newspaper.json";
+        break;
+    }
+
+    fetch(animationFile)
+      .then((res) => res.json())
+      .then((data) => setAnimationData(data))
+      .catch((err) => console.error(`Error al cargar ${type}.json`, err));
+  }, [type]);
+
+  if (!animationData) return <p>Cargando...</p>;
+
+  return (
+    <Lottie
+      animationData={animationData}
+      loop={true}
+      style={{ width: size, height: size }}
+    />
+  );
+};
+
+export default AnimationIcons;
