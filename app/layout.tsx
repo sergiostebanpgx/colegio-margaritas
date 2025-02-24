75ed697726b91eca2525fc1a79e9d340590c216c
sergiostebanpgx <sergiosteban700@gmail.com>
2025-02-24 14:58:02 -0500

    Update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Footer from "./components/Footer"
 import WhatsAppButton from "./components/WhatsAppButton"
 import type React from "react" // Import React
 import PageTransition from "./components/PageTransition"
+import type { Metadata } from 'next'
 
 const poppins = Poppins({
   weight: ["400", "600", "700"],
@@ -12,9 +13,12 @@ const poppins = Poppins({
   display: "swap",
 })
 
-export const metadata = {
-  title: "Colegio Las Margaritas",
+export const metadata: Metadata = {
+  title: "Colegio Las Margaritas | Alianza Educativa",
   description: "Sitio web oficial del Colegio Las Margaritas",
+  icons: {
+    icon: '/favicon.ico',
+  }
 }
 
 export default function RootLayout({
@@ -24,6 +28,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es" className={poppins.className}>
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow">
